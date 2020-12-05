11cae0c9063dcb3c31c9fefdf5308164db0c369f
Cameron Mackintosh <cbmackintosh@outlook.com>
2020-12-05 04:48:43 -0700

    complete shelf.js all tests passed

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,42 @@
 
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+}
+
+function unshelfBook(book, shelf) {
+  shelf.splice(shelf.indexOf(book.title) - 1, 1);
+}
+
+function listTitles(shelf) {
+  var result = '';
+  for(i = 0; i < shelf.length; i++) {
+    if (i === shelf.length - 1) {
+      result = result + shelf[i].title;
+    } else {
+      result = result + shelf[i].title + ', ';
+    }
+  }
+  return result;
+}
+
+function searchShelf(shelf, book) {
+  var result = null;
+  for (i = 0; i < shelf.length; i ++) {
+    if (shelf[i].title === book) {
+      result = true;
+      break;
+    } else {
+      result = false;
+    }
+  }
+  return result;
+}
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
